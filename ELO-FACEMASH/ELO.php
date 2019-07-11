434b3c4e547decb4196552575756dfd6aa87dbac
Russell Rounds <rdoubleoc@aol.com>
2019-07-11 00:20:57 -0400

    Started ELO ALgorithm Exploration

--- ELO-FACEMASH/ELO.php
+++ ELO-FACEMASH/ELO.php
@@ -1,11 +1,13 @@
+
 <?php
 header('content-type: application/json; charset=utf-8');
+print "<html>";
 
 function ELO($A, $B){ // From http://en.wikipedia.org/wiki/Elo_rating_system
-        return (1 / (1+pow(10,(($B-$A)/400))));
+        return (1/(1+pow(10,(($B-$A)/400))));
 };
 
 //All code below this written by Russell Rounds
 $eloResult = ELO(100,200);
-print_r($eloResult);
+echo '<br/>' . 'Value of $eloResult: ' . print_r($eloResult);
 ?>

00b8b4a2e71f13f763937d9656873c580013f87e
Russell Rounds <rdoubleoc@aol.com>
2019-07-10 23:40:21 -0400

    Update ELO Function.php

--- ELO-FACEMASH/ELO Function.php
+++ ELO-FACEMASH/ELO Function.php
@@ -1,5 +1,11 @@
-<?php 
-function ELO($A, $B){ // the ELO formula taken from http://en.wikipedia.org/wiki/Elo_rating_system
-        return (1/(1+pow(10,(($B-$A)/400))));
-}
+<?php
+header('content-type: application/json; charset=utf-8');
+
+function ELO($A, $B){ // From http://en.wikipedia.org/wiki/Elo_rating_system
+        return (1 / (1+pow(10,(($B-$A)/400))));
+};
+
+//All code below this written by Russell Rounds
+$eloResult = ELO(100,200);
+print_r($eloResult);
 ?>

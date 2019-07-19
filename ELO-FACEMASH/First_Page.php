454740f4a2a7213754a0fc9bf4f35cb0e92f01eb
Russell Rounds <rdoubleoc@aol.com>
2019-07-19 14:05:44 -0400

    Score Picking and File/Score Reading Working

--- ELO-FACEMASH/First_Page.php
+++ ELO-FACEMASH/First_Page.php
@@ -11,8 +11,8 @@
 <?php
 include('functions.php');
 $DIR = 'SampleData';
-echo $MAXNUM_Files_in_DIR = count_files_in_DIR($DIR);
-echo '<br/>';
+$MAXNUM_Files_in_DIR = count_files_in_DIR($DIR);
+echo 'Max Files in Data Directory: ' . $MAXNUM_Files_in_DIR . '<br/>';
 
 //Choose Player Files
 $Player1 = RAND(1,$MAXNUM_Files_in_DIR);
@@ -25,13 +25,22 @@ echo 'Player 1 Name = ' . $Player1;
 echo '<br/>';
 echo 'Player 2 Name = ' . $Player2;
 echo '<br/>';
+
 $Player1_filename = $DIR . '/' . $Player1 . '.txt';
+$Player2_filename = $DIR . '/' . $Player2 . '.txt';
+
 echo 'Player 1 File: ' . $Player1_filename;
-
 echo '<br/>';
+echo 'Player 2 File: ' . $Player2_filename;
 
-echo $Player1_currentScore = read($Player1_filename);
+echo '<br/>';echo '<br/>';
 
+
+$Player1_currentScore = read($Player1_filename);
+$Player2_currentScore = read($Player2_filename);
+echo '<strong>Player 1 Score: ' . $Player1_currentScore . '</strong>';
+echo '<br/>';
+echo '<strong>Player 2 Score: ' . $Player2_currentScore . '</strong>';
 
 //Other Functions
 function ELO($A, $B){

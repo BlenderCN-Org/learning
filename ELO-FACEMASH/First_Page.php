09515afffe95ac8a236c5bf7f7c6f868fab531b3
Russell Rounds <rdoubleoc@aol.com>
2019-07-19 15:20:27 -0400

    Ready for ELO to be applied, but working

--- ELO-FACEMASH/First_Page.php
+++ ELO-FACEMASH/First_Page.php
@@ -20,41 +20,48 @@ $Score_DIR = 'Actress_Scores';
 $Info_DIR = 'Actress_Info';
 
 //Choose Players
-$MAX_NUM_Files_in_DIR = count_files_in_DIR($Score_DIR);
-$Player1 = RAND(1,$MAX_NUM_Files_in_DIR);
-$Player2 = RAND(1,$MAX_NUM_Files_in_DIR);
+$NUM_Files_in_DIR = count_files_in_DIR($Score_DIR);
+$Player1 = RAND(1,$NUM_Files_in_DIR);
+$Player2 = RAND(1,$NUM_Files_in_DIR);
 while ($Player1 === $Player2){ // To avoid players being the same
-	$Player2 = RAND(1,$MAX_NUM_Files_in_DIR);
+	$Player2 = RAND(1,$NUM_Files_in_DIR);
 };
+
+$Player1_filename = $Score_DIR . '/' . $Player1 . '.txt';
+$Player2_filename = $Score_DIR . '/' . $Player2 . '.txt';
+$Player1_picture_filename = $Info_DIR . '/' . 'Pictures/' . $Player1 . '.jpg';
+$Player2_picture_filename = $Info_DIR . '/' . 'Pictures/' . $Player2 . '.jpg';
 
 //Print Initial Output (before ELO applied)
 echo 'Data DIR Name = ' . $Score_DIR;
 echo '<br/>';
-echo 'Max Files in Data Directory: ' . $MAX_NUM_Files_in_DIR;
+echo 'Max Files in Data Directory: ' . $NUM_Files_in_DIR;
 echo '<br/>';
 echo 'Player 1 Name = ' . $Player1;
 echo '<br/>';
 echo 'Player 2 Name = ' . $Player2;
 echo '<br/>';
-
-$Player1_filename = $Score_DIR . '/' . $Player1 . '.txt';
-$Player2_filename = $Score_DIR . '/' . $Player2 . '.txt';
-
 echo 'Player 1 File: ' . $Player1_filename;
 echo '<br/>';
 echo 'Player 2 File: ' . $Player2_filename;
-echo '<br/>';echo '<br/>';
+echo '<br/>';
+echo '<br/>';
+echo 'Player 1 Picure Path: ' . $Player1_picture_filename;
+echo '<br/>';
+echo 'Player 2 Picure Path: ' . $Player2_picture_filename;
+echo '<br/>';
 
-//Read scores from text files
+//Read Scores from Files
 $Player1_currentScore = read($Player1_filename);
 $Player2_currentScore = read($Player2_filename);
+
+//Display Pictures
 echo '<strong>Player 1 Score: ' . $Player1_currentScore . '</strong>';
 echo '<br/>';
 echo '<strong>Player 2 Score: ' . $Player2_currentScore . '</strong>';
 echo '<br/>';
-echo $Player1_picture_filename = $Info_DIR . '/' . 'Pictures/' . $Player1 . '.jpg';
-echo '<br/>';
-echo '<img src="' . $Player1_picture_filename . '">';
+echo '<img src="' . $Player1_picture_filename . '" width="25%" height="25%">';
+echo '<img src="' . $Player2_picture_filename . '" width="25%" height="25%">';
 
 //Other Functions
 function ELO($A, $B){

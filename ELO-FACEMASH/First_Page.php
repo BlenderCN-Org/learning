00d7733e7971be96b73a3ea061336b882aa75a57
Russell Rounds <rdoubleoc@aol.com>
2019-07-19 15:05:09 -0400

    Working
    Displays 1 Picture
    Added ability to add picture data, such as name (before it was going to be picture and score only, since the algorithm only needs the score and the user only needs the picture).

--- ELO-FACEMASH/First_Page.php
+++ ELO-FACEMASH/First_Page.php
@@ -9,40 +9,52 @@
 <h2>ELO Matching Example/Experiment</h2><br/>
 
 <?php
-require_once('functions.php');
-$DIR = 'Actress_Scores';
-$MAXNUM_Files_in_DIR = count_files_in_DIR($DIR);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-//Choose Player Files
-$Player1 = RAND(1,$MAXNUM_Files_in_DIR);
-$Player2 = RAND(1,$MAXNUM_Files_in_DIR);
+require_once('functions.php');
+
+//Configurable Variables
+$Score_DIR = 'Actress_Scores';
+$Info_DIR = 'Actress_Info';
+
+//Choose Players
+$MAX_NUM_Files_in_DIR = count_files_in_DIR($Score_DIR);
+$Player1 = RAND(1,$MAX_NUM_Files_in_DIR);
+$Player2 = RAND(1,$MAX_NUM_Files_in_DIR);
 while ($Player1 === $Player2){ // To avoid players being the same
-	$Player2 = RAND(1,$MAXNUM_Files_in_DIR);
+	$Player2 = RAND(1,$MAX_NUM_Files_in_DIR);
 };
 
-echo 'Data DIR Name = ' . $DIR;
+//Print Initial Output (before ELO applied)
+echo 'Data DIR Name = ' . $Score_DIR;
 echo '<br/>';
-echo 'Max Files in Data Directory: ' . $MAXNUM_Files_in_DIR;
+echo 'Max Files in Data Directory: ' . $MAX_NUM_Files_in_DIR;
 echo '<br/>';
 echo 'Player 1 Name = ' . $Player1;
 echo '<br/>';
 echo 'Player 2 Name = ' . $Player2;
 echo '<br/>';
 
-$Player1_filename = $DIR . '/' . $Player1 . '.txt';
-$Player2_filename = $DIR . '/' . $Player2 . '.txt';
+$Player1_filename = $Score_DIR . '/' . $Player1 . '.txt';
+$Player2_filename = $Score_DIR . '/' . $Player2 . '.txt';
 
 echo 'Player 1 File: ' . $Player1_filename;
 echo '<br/>';
 echo 'Player 2 File: ' . $Player2_filename;
 echo '<br/>';echo '<br/>';
 
-
+//Read scores from text files
 $Player1_currentScore = read($Player1_filename);
 $Player2_currentScore = read($Player2_filename);
 echo '<strong>Player 1 Score: ' . $Player1_currentScore . '</strong>';
 echo '<br/>';
 echo '<strong>Player 2 Score: ' . $Player2_currentScore . '</strong>';
+echo '<br/>';
+echo $Player1_picture_filename = $Info_DIR . '/' . 'Pictures/' . $Player1 . '.jpg';
+echo '<br/>';
+echo '<img src="' . $Player1_picture_filename . '">';
 
 //Other Functions
 function ELO($A, $B){

9d986241c82267452abef9503578750cfcb01b3d
Russell Rounds <rdoubleoc@aol.com>
2019-07-20 01:00:46 -0400

    Misc

--- ELO-FACEMASH/First_Page.php
+++ ELO-FACEMASH/First_Page.php
@@ -29,7 +29,7 @@ if(isset($_POST['Winners']) and $_SERVER['REQUEST_METHOD'] === "POST"){
 	$LoserScoreFilename = $Score_DIR . $Previous_Loser . '.txt';
 	$Loser_Old_Score = read($LoserScoreFilename);
 	$points_won_by_winner = $Loser_Old_Score + 400; // https://en.wikipedia.org/wiki/Elo_rating_system#FIDE_ratings
-	$points_lost_by_loser = $Winner_Old_Score - 400; // Is this right?  How can it be?
+	//$points_lost_by_loser = $Winner_Old_Score - 400; // Is this right?  How can it be?
 	
 	//Update scores for both players
 	$WinnerTotalPoints = $Winner_Old_Score + $points_won_by_winner;
@@ -37,10 +37,10 @@ if(isset($_POST['Winners']) and $_SERVER['REQUEST_METHOD'] === "POST"){
 	echo '<font color="green"><strong>Winner score updated!</font></strong>';
 	echo '<br/>';
 	
-	$LoserTotalPoints = $Loser_Old_Score - $points_lost_by_loser;
+	/* $LoserTotalPoints = $Loser_Old_Score - $points_lost_by_loser;
 	write($LoserScoreFilename, $LoserTotalPoints);
 	echo '<font color="green"><strong>Loser score updated!</font></strong>';
-	echo '<br/>';
+	echo '<br/>'; */
 
 	
 	//Debugging Info

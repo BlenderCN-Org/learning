317fd29a95cb8d40b28812109b012aa2b5dd5941
Russell Rounds <rdoubleoc@aol.com>
2019-08-15 20:38:28 -0400

    Update index_dopple.php

--- ELO-FACEMASH/index_dopple.php
+++ ELO-FACEMASH/index_dopple.php
@@ -14,7 +14,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once('functions.php');
-	
+
+// To Do:
+// - Move all styling to separate file.
+
 //Configurable Variables
 $Player_LOCKED = FALSE;
 $DEBUG = FALSE;

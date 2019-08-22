7170cbf7bb630852b2ee7582f48abd380e25454f
Russell Rounds <rdoubleoc@aol.com>
2019-08-22 01:46:31 -0400

    Update test_POST_and_array_filtering.php
    Created filter function with ability to take exceptions.

--- ELO-FACEMASH/test_POST_and_array_filtering.php
+++ ELO-FACEMASH/test_POST_and_array_filtering.php
@@ -6,10 +6,13 @@ ob_implicit_flush(true);
 require_once('functions.php');
 
 	$ar = array();
-	$ar = array_push_a($ar, 'A', '(');
+	$ar = array_push_a($ar, 'A', 'A');
 	$ar = array_push_a($ar, 'B', ')');
-	$ar = array_push_a($ar, 'C', ',');
-	$ar = array_push_a($ar, 'D', 4);
+	$ar = array_push_a($ar, 'C', '(');
+	$ar = array_push_a($ar, 'D', '4');
+	$ar = array_push_a($ar, 'E', 4); // Numbers are not considered alphanumeric? odd.
+	$ar = array_push_a($ar, 'F', ',');
+	$ar = array_push_a($ar, 'G', '?');
 
 	echo '<pre>';
 	echo '<br/>Print_r:<br/>';
@@ -17,10 +20,12 @@ require_once('functions.php');
 	echo '<br/>';
 	echo '</pre>';
 	echo '<br/>';
-	if(filter_array($ar) == TRUE){
+	$array_filtered = filter_array($ar);
+	echo '<br/>';
+	if($array_filtered == TRUE){
 		echo 'filter_array output: TRUE';
 	}else{
-		if(filter_array($ar) === FALSE){
+		if($array_filtered === FALSE){
 			echo 'filter_array output: FALSE (===)<br/>';
 		}else{
 			echo 'filter_array output: FALSE (==)<br/>';

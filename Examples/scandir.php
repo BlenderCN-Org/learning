1935c641994a323ccd96171659d8fbb7051803c2
Russell Rounds <rdoubleoc@aol.com>
2019-09-01 00:19:20 -0400

    File Man.

--- Examples/scandir.php
+++ Examples/scandir.php
@@ -3,11 +3,11 @@ $cwd = getcwd();
 $dir  = $cwd;
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
-//echo $cwd;
+echo $cwd;
 echo $dir;
 echo '<pre>';
-//print_r($files1);
-//print_r($files2);
+print_r($files1);
+print_r($files2);
 echo '</pre>';
 
 // $dir = "/tmp";
@@ -22,7 +22,7 @@ echo '</pre>';
 // echo '</pre>';
 foreach ($files1 as $file){
 if (substr($file, -4) == ".jpg"){
-//echo '<a href="' . $file . '">' . $file . '</a><br/>';
+echo '<a href="' . $file . '">' . $file . '</a><br/>';
 echo '<img src="' . $file . '" width="100" height="100"/><br/>';
 
 

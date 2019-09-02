c7b6ad4e98f234950b76d8fd47905afd16fdda8d
Russell Rounds <rdoubleoc@aol.com>
2019-09-02 01:25:33 -0400

    Various Different Stuff

--- Code/PHP/test.php
+++ Code/PHP/test.php
@@ -2,6 +2,7 @@
 	// BUG: If an automatic global (superglobal) is not used, it may not be present in $GLOBALS
 	// See: https://bugs.php.net/bug.php?id=65223&edit=2
 	// See also: https://www.php.net/manual/en/reserved.variables.globals.php
+	
 	$_SERVER; // Comment this in/out to see how output changes
 	echo '<pre>';
 	print_r($GLOBALS);
